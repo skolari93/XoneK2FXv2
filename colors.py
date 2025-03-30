fdffe3d609c0d51595bc58eb64ea1d1e0ee78274
manuel <manuel.stathis@pm.me>
2025-03-31 00:17:25 +0200

    colors seem to work

--- colors.py
+++ colors.py
@@ -1,68 +1,11 @@
 from ableton.v3.control_surface.elements import Color, SimpleColor
 from ableton.v2.control_surface import MIDI_PB_TYPE
-CHANNEL = 15
-import logging
-logger = logging.getLogger("XoneK2FXv2")
-# RED = 0
-# AMBER = 36
-# GREEN = 72
-BLACK = 1
-
-
-RED = 0
-AMBER = 4
-GREEN = 8
-BLACK = 666
-DEFAULTVALUE=127
-
-class K2Color(Color):
-    def __init__(self, value, color=RED, channel=None, *a, **k):
-        (super().__init__)(*a, **k)
-        self._value = value
-        self._channel = channel
-        self._color = color
-
-    @property
-    def midi_value(self):
-        return self._value
-
-    def send_color(value, interface, channel=CHANNEL,color=RED):
-        data_byte1 = interface._original_identifier + color # <---------------
-        data_byte2 = value
-        status_byte = interface._status_byte(interface._original_channel if channel is None else channel)
-        if interface._msg_type == MIDI_PB_TYPE:
-            data_byte1 = value & 127
-            data_byte2 = value >> 7 & 127
-        if interface.send_midi((status_byte, data_byte1, data_byte2)):
-            interface._last_sent_message = (
-             value, channel)
-            if interface._report_output:
-                is_input = True
-                interface._report_value(value, not is_input)
-        interface.send_value((value), channel=(channel))        
-
-    def draw(self, interface):
-        data_byte1 = interface._original_identifier + self._color # <---------------
-        data_byte2 = self._value
-        status_byte = interface._status_byte(interface._original_channel if self._channel is None else self._channel)
-        if interface._msg_type == MIDI_PB_TYPE:
-            data_byte1 = self._value & 127
-            data_byte2 = self._value >> 7 & 127
-        if interface.send_midi((status_byte, data_byte1, data_byte2)):
-            interface._last_sent_message = (
-             self._value, self._channel)
-            if interface._report_output:
-                is_input = True
-                interface._report_value(self._value, not is_input)
-        interface.send_value((self._value), channel=(self._channel))
+from .k2_button import RED, AMBER, GREEN, BLACK
 
 
 
 class Rgb:
-    logger.info("rgb init")
-
-    amber = K2Color(127,color=AMBER)
-    green = K2Color(127,color=GREEN)
-    red = K2Color(127,color=RED)
-    black = K2Color(0, color=GREEN)
-    # black = SimpleColor(0)
+    red = SimpleColor(RED)
+    green = SimpleColor(GREEN)
+    amber = SimpleColor(AMBER)
+    black = SimpleColor(BLACK)
--- k2_button.py
+++ k2_button.py
@@ -1,46 +1,84 @@
 from ableton.v3.control_surface.elements import ButtonElement
 from ableton.v2.control_surface import MIDI_PB_TYPE, MIDI_NOTE_TYPE
 import logging
+
 logger = logging.getLogger("XoneK2FXv2")
-# RED = 0
-# AMBER = 36
-# GREEN = 72
-# BLACK = 1
 
-RED = 0
-AMBER = 4
-GREEN = 8
+# OFFSETS SMALL BUTTONS
+REDSMALLBUTTONS = 0
+AMBERSMALLBUTTONS = 36
+GREENSMALLBUTTONS = 72
 
+# OFFSETS BIG BUTTONS
+REDBIGBUTTONS = 0
+AMBERBIGBUTTONS = 4
+GREENBIGBUTTONS = 8
+
+# Selector Integers
+RED = 1
+AMBER = 2
+GREEN = 3
+BLACK = 4
 CHANNEL = 14
 
 class K2ButtonElement(ButtonElement):
-    def __init__(self, identifier, channel=0, msg_type=MIDI_NOTE_TYPE, is_momentary=True, led_channel=None, **k):
+    def __init__(self, identifier, button_type="small", channel=0, msg_type=MIDI_NOTE_TYPE, is_momentary=True, led_channel=None, **k):
         super().__init__(identifier, channel, msg_type, is_momentary, led_channel, **k)
+        self._button_type = button_type
 
-    def _do_send_value(self, value, channel = CHANNEL, color=RED):
-        # if int(value) == 1: 
-        #     data_byte2 = 0
+    def get_color(self, color_selector):
+        """Returns the corresponding MIDI value for the given color and button type."""
+        if self._button_type == "small":
+            if color_selector == RED:
+                return REDSMALLBUTTONS
+            elif color_selector == AMBER:
+                return AMBERSMALLBUTTONS
+            elif color_selector == GREEN:
+                return GREENSMALLBUTTONS
+        elif self._button_type == "big":  # "big" button type
+            if color_selector == RED:
+                return REDBIGBUTTONS
+            elif color_selector == AMBER:
+                return AMBERBIGBUTTONS
+            elif color_selector == GREEN:
+                return GREENBIGBUTTONS
+        return 0  # Default case if an invalid color is given
 
-        data_byte1 = self._original_identifier + value # <---------------
-        data_byte2 = 0
-        if value == 666: 
-            data_byte2 = 0
+    def _send_midi_message(self, value, channel, color):
+        """Helper method to send MIDI messages."""
+        data_byte1 = self._original_identifier + int(color)
+        data_byte2 = value
         status_byte = self._status_byte(self._original_channel if channel is None else channel)
+
         if self._msg_type == MIDI_PB_TYPE:
             data_byte1 = value & 127
-            data_byte2 = value >> 7 & 127
+            data_byte2 = (value >> 7) & 127
+
         if self.send_midi((status_byte, data_byte1, data_byte2)):
-            self._last_sent_message = (
-             value, channel)
+            self._last_sent_message = (value, channel)
             if self._report_output:
                 is_input = True
                 self._report_value(value, not is_input)
+
+    def _do_send_value(self, value, channel=None):
+        """Handles sending the value."""
+        logger.info(value)
+
+        if value == BLACK:
+            velocity = 0
+            self._send_midi_message(velocity, channel, self.get_color(RED))
+            self._send_midi_message(velocity, channel, self.get_color(GREEN))
+            self._send_midi_message(velocity, channel, self.get_color(AMBER))
+        else:
+            velocity = 127  # Example: Set velocity if the value is even
+            self._send_midi_message(velocity, channel, self.get_color(value))
 
 
 def create_k2_button(identifier, **k):
     return K2ButtonElement(
         identifier,
-        k.pop("channel", CHANNEL),  # Default to channel 0 if not provided
+        k.pop("button_type", "small"),  # Default to "small" button
+        k.pop("channel", CHANNEL),  # Default to predefined CHANNEL
         k.pop("msg_type", MIDI_NOTE_TYPE),  # Default msg_type
         k.pop("is_momentary", True),  # Default to momentary button
         k.pop("led_channel", None),  # Default None
