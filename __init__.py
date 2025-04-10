719bf267e4d74a4714bf4976c559eba009b165f8
manuel <manuel.stathis@pm.me>
2025-04-10 23:05:37 +0200

    rm comments

--- __init__.py
+++ __init__.py
@@ -1,16 +1,4 @@
 from .XoneK2FXv2 import XoneK2FXv2, Specification
-# from . import control
-
-# from ableton.v3.control_surface import (
-#     ControlSurfaceSpecification,
-#     create_skin,
-# )
-
-
-# class Specification(ControlSurfaceSpecification):
-#     control_surface_skin = create_skin(skin=control.Skin)
-
-
 
 def create_instance(c_instance):
     return XoneK2FXv2(Specification, c_instance=c_instance)
